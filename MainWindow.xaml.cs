cf11c47715f602bd88d091680c2e3e792f3334fb
EduardoMoraton <90452631+EduardoMoraton@users.noreply.github.com>
2022-09-26 13:31:22 +0200

    Agregar archivos de proyecto.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace HolaMundo
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void saludarButton_Click(object sender, RoutedEventArgs e)
+        {
+            holamundoTextBlock.Text = "Hola " + input1TextBox.Text ?? "Mundo!";
+        }
+    }
+}
